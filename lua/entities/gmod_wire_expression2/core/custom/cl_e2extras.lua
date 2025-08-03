88a639ed7379fdfb7e79002b27a9387c1904e880
Jimmy King <47538595+jimmyking2004@users.noreply.github.com>
2025-08-03 00:35:50 -0500

    Update cl_e2extras.lua
    Added notes about using the built-in variant of the same function in Wiremod

--- lua/entities/gmod_wire_expression2/core/custom/cl_e2extras.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_e2extras.lua
@@ -1,16 +1,16 @@
 
-// General Entity Functions
+-- General Entity Functions
 E2Helper.Descriptions["getGroundEntity"] = "Gets the entity that a player/NPC is standing on. [Extra E2 Functions]"
 E2Helper.Descriptions["isMapMade"] = "Is the entity created by the map? [Extra E2 Functions]"
-E2Helper.Descriptions["timeCreated"] = "Gets the time that the entity was created in relation server start. [Extra E2 Functions]"
+E2Helper.Descriptions["timeCreated"] = "Use built-in creationTime() if available! as it's the same. Gets the time that the entity was created in relation server start. [Extra E2 Functions]"
 E2Helper.Descriptions["hasBoneManipulations"] = "Can the entities bones be manipulated? [Extra E2 Functions]"
 E2Helper.Descriptions["isRagdoll"] = "Is the entity a ragdoll? [Extra E2 Functions]"
 E2Helper.Descriptions["mapCreationID"] = "Gets the map creation ID of an entity, this ID wont change unless the map is edited. Returns -1 if the entity is not created by the map. [Extra E2 Functions]"
 E2Helper.Descriptions["removeDecals"] = "Removes all decals from the entity. (Crowbars are no longer a nuisance!) [Extra E2 Functions]"
 E2Helper.Descriptions["isPersistent"] = "Is the entity persistent? [Extra E2 Functions]"
-E2Helper.Descriptions["setVel"] = "Sets the entities velocity. [Extra E2 Functions]"
+E2Helper.Descriptions["setVel"] = "Use Propcore propSetVelocity() if available! Sets the entities velocity. [Extra E2 Functions]"
 E2Helper.Descriptions["setLocalVel"] = "Sets the entities local velocity. [Extra E2 Functions]"
-E2Helper.Descriptions["setInstVel"] = "Sets the entities velocity instantaneously. [Extra E2 Functions]"
+E2Helper.Descriptions["setInstVel"] = "Use Propcore propSetVelocityInstant() if available! Sets the entities velocity instantaneously. [Extra E2 Functions]"
 E2Helper.Descriptions["waterLevel"] = "How deep is the entity in the water? 0 - Not in water. | 1 - Touching in water. | 2 - Half in water. | 3 - Fully under water. [Extra E2 Functions]"
 E2Helper.Descriptions["keepUpright"] = "Apply keep upright to the entity. Args: (BoneID, Strength) [Extra E2 Functions]"
 E2Helper.Descriptions["stress"] = "Gets the physical stress being applied on the entity. [Extra E2 Functions]"
@@ -18,12 +18,12 @@ E2Helper.Descriptions["getAnimID"] = "Gets the current animation ID of the entit
 E2Helper.Descriptions["getAnimList"] = "Gets the entities list of animations as strings. [Extra E2 Functions]"
 E2Helper.Descriptions["setUnbreakable"] = "Set if the entity can be broken by physics. [Extra E2 Functions]"
 
-// Player Functions
+-- Player Functions
 E2Helper.Descriptions["canUseFlashlight"] = "Can the player use their flashlight? [Extra E2 Functions]"
 E2Helper.Descriptions["getHands"] = "Gets the players hands. [Extra E2 Functions]"
 E2Helper.Descriptions["canColideWithTeam"] = "Can the player collide with team mates? [Extra E2 Functions]"
 E2Helper.Descriptions["getRagdoll"] = "Gets the players death ragdoll. [Extra E2 Functions]"
-E2Helper.Descriptions["getWeapons"] = "Gets a list of weapons this player has. [Extra E2 Functions]"
+E2Helper.Descriptions["getWeapons"] = "Use built-in weapons() if available! as it's the same. Gets a list of weapons this player has. [Extra E2 Functions]"
 E2Helper.Descriptions["isBot"] = "Is the player a bot? [Extra E2 Functions]"
 E2Helper.Descriptions["isConnected"] = "Is the player (entity) connected to the server?. [Extra E2 Functions]"
 E2Helper.Descriptions["isAuth"] = "Is the player authenticated with steam network? [Extra E2 Functions]"
@@ -31,5 +31,5 @@ E2Helper.Descriptions["isHostPlayer"] = "Is this player hosting the local server
 E2Helper.Descriptions["steamID64"] = "Gets the 64 bit version of the players steam ID. [Extra E2 Functions]"
 E2Helper.Descriptions["killOwner"] = "Kills the owner of the E2. [Extra E2 Functions]"
 E2Helper.Descriptions["held"] = "Returns the last entity the player attempted to physgun. [Extra E2 Functions]"
-E2Helper.Descriptions["freezeOwner"] = "Freezes the owner preventing control such as WASD and mouse movement, removing the initial E2 that froze you will automatically unfreeze you if you get stuck. [Extra E2 Functions]"
-E2Helper.Descriptions["inGod"] = "Is the player in god mode? [Extra E2 Functions]"
+E2Helper.Descriptions["freezeOwner"] = "Freezes the owner preventing control, removing the initial E2 that froze you will automatically unfreeze you if you get stuck. [Extra E2 Functions]"
+E2Helper.Descriptions["inGod"] = "Use built-in inGodMode() if available! as it's the same. Is the player in god mode? [Extra E2 Functions]"
